34c2a5b27914881d6022d975c3a295ed9788f91d
Yury Mediro <sagn375@gmail.com>
2024-08-20 19:45:17 +0300

    todo list 1

--- js/main.js
+++ js/main.js
@@ -125,24 +125,25 @@ function addTodo(event) {
 	//Todo DIV
 	const todoDiv = document.createElement('div')
 	todoDiv.classList.add('todo')
+	//Check mark button
+
+	const completeButton = document.createElement('button')
+	completeButton.innerHTML = '<i class=" fa-check"></i>'
+	completeButton.classList.add('complete-btn')
+	todoDiv.appendChild(completeButton)
 	//Create li
 	const newTodo = document.createElement('li')
 	newTodo.innerText = todoInput.value
 	newTodo.classList.add('todo-item')
 	todoDiv.appendChild(newTodo)
-	//Check mark button
-	const completeButton = document.createElement('button')
-	completeButton.innerHTML = '<i class="fas fa-check"></i>'
-	completeButton.classList.add('complete-btn')
-	todoDiv.appendChild(completeButton)
 	//Check edit button
 	const editButton = document.createElement('button')
-	editButton.innerHTML = '<i class="fas fa-edit"></i>'
+	editButton.innerHTML = '<i class=" fa-edit"></i>'
 	editButton.classList.add('edit-btn')
 	todoDiv.appendChild(editButton)
 	//Check trash button
 	const trashButton = document.createElement('button')
-	trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+	trashButton.innerHTML = '<i class="fa-trash"></i>'
 	trashButton.classList.add('trash-btn')
 	todoDiv.appendChild(trashButton)
 	//APPEND TO LIST
